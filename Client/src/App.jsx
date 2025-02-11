d84c236eb66a0c5009f397d4a2288a56bf3ce192
philip0980 <philipchaudhary190@gmail.com>
2025-02-12 04:01:00 +0530

    minor change in frontend

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import FileUpload from "./FileUpload";
 
 const App = () => {
   return (
     <div>
-      <h1>Sangeet Manch : A stage for music lovers</h1>
-      <FileUpload />
+      <h1>Coming Soon</h1>
     </div>
   );
 };
